4ffae7b4e44555701240af0a5b3fd359545ad91c
Mike Lorey <mdlorey@gmail.com>
2022-02-28 15:20:40 -0500

    building out a basic timer app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,74 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h2>Timer Application</h2>
+    <label>Input a Date to count to</label><br />
+    <input type="text" v-model="dateInput" placeholder="mm/dd/yyyy hh:mm" /><br />
+    <button @click="countdown()">Count Up/Down</button>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      {{ timerDisplay }}
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      timerDisplay: null,
+      dateInput: '03/01/2022 14:46',
+      // dateInput: '02/27/2022 14:46',
+      countdownInterval: null
+    }
+  },
+  mounted() {
+    // this.countdown(10);
+
+    // console.log(this.formatDate(new Date()));
+  },
+  methods: {
+    formatDate(value) {
+      if (value) {
+        return moment(String(value)).format('MM/DD/YYYY');
+      }
+    },
+    calculateDuration() {
+      // console.log(new Date(this.dateInput));
+      const now = moment(new Date());
+      const then = moment(new Date(this.dateInput));
+      return now.diff(then, 'seconds');
+      // console.log(now.diff())
+    },
+    // plain date and text time input
+    // count up or down from time provided
+
+    // mm/dd/yyy - hh:mm
+    countdown() {
+      let duration = Math.abs(this.calculateDuration());
+
+      let timer = duration, minutes, seconds;
+      this.clearTheInterval();
+      this.countdownInterval = setInterval(() => {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        this.timerDisplay = `${minutes}:${seconds}`;
+        if (--timer < 0) {
+          timer = duration;
+        }
+      }, 1000);
+    },
+    clearTheInterval() {
+      clearInterval(this.countdownInterval);
+    }
   }
 }
 </script>
